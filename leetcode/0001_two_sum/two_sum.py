2274a1aa0338feaac76bb8404a8728b498c44022
Vinoth Raj <vinoth@Vinoths-MacBook-Air.local>
2024-02-24 16:17:06 +0530

    #0001 two sum

--- leetcode/0001_two_sum/two_sum.py
+++ leetcode/0001_two_sum/two_sum.py
@@ -1,5 +1,4 @@
 """
-
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
@@ -13,7 +12,6 @@ Constraints:
 2 <= nums.length <= 10^4
 -10^9 <= nums[i] <= 10^9
 -10^9 <= target <= 10^9
-
 """
 
 from typing import List
@@ -31,34 +29,31 @@ class Solution:
 
 
 class TestSolution(unittest.TestCase):
-    def test1_two_sum(self):
-        solution = Solution()
 
-        # simple case with postive integers
+    def test1_two_sum(self):
+        """Simple case with postive integers."""
+        solution = Solution()
         nums = [2, 7, 11, 15]
         target = 9
         self.assertEqual(solution.two_sum(nums, target), [0, 1])
 
-    # case with negative integers
     def test2_two_sum(self):
+        """Case with negative integers."""
         solution = Solution()
-
         nums = [-4, -2, -8, -1]
         target = -3
         self.assertEqual(solution.two_sum(nums, target), [1, 3])
 
-    # case with positive and negative integers
     def test3_two_sum(self):
+        """Case with positive and negative integers."""
         solution = Solution()
-
         nums = [-4, 2, 8, -1]
         target = 4
         self.assertEqual(solution.two_sum(nums, target), [0, 2])
 
-    # edge case
     def test4_two_sum(self):
+        """edge case."""
         solution = Solution()
-
         nums = [10**9, 10**9 - 1]
         target = 2*10**9 - 1
         self.assertEqual(solution.two_sum(nums, target), [0, 1])
