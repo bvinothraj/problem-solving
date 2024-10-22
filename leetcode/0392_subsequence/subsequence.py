ec3dce3516467eba1566c696c6bbbd5a9800c350
Vinoth Raj <vinoth@Vinoths-MacBook-Air.local>
2024-10-22 06:33:13 +0530

    0392 leet code subsequence

--- leetcode/0392_subsequence/subsequence.py
+++ leetcode/0392_subsequence/subsequence.py
@@ -4,8 +4,8 @@ LeetCode: 0392 Is Subsequence
 https://leetcode.com/problems/is-subsequence/description/
 
 Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
-A subsequence of a string is a new string that is formed from the original string by 
-deleting some (can be none) of the characters without disturbing the relative positions 
+A subsequence of a string is a new string that is formed from the original string by
+deleting some (can be none) of the characters without disturbing the relative positions
 of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 Example 1:
@@ -25,6 +25,10 @@ import unittest
 
 class Solution:
     def is_subsequence(self, s, t):
+        """
+        Time Complexity: O(n) where n is length of string t
+        Space Complexity: O(1)
+        """
         if not s:  # empty string is a subsequence
             return True
 
